ea77adb3e76b770314d190662f0cac1949db683f
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-08-18 15:12:43 +0530

    modified

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,6 +3,7 @@ const { MONGODB_URL } = require('./serverConfig');
 
 async function connectToDB(){
     try{
+        console.log(MONGODB_URL)
         await mongoose.connect(MONGODB_URL);
     } catch (err){
         console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const app = require('./app');
 const serverConfig = require('./config/serverConfig');
-const fastify = require('fastify')({logger: true})
+const fastify = require('fastify')({logger: true});
+const {connectToDB} = require('./config/dbConfig')
 
 const PORT = serverConfig.PORT
 fastify.register(app);
 
-fastify.listen({port: PORT}, (err)=> {
+fastify.listen({port: PORT}, async (err)=> {
     if(err){
         fastify.log.error(err);
         process.exit(1);
     }
+    await connectToDB();
     console.log(`Server is up at port ${PORT}`);
 })
